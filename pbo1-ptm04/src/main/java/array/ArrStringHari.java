d89299d389badae6329f0e92fda76440f23f580b
Kissaputera <mkissaputera@gmail.com>
2023-05-05 20:45:48 +0800

    Create ArrStringHari.java
    penambahan class baru, Array String Hari pada Array package

---
+++ pbo1-ptm04/src/main/java/array/ArrStringHari.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package array;
+
+/**
+ *
+ * @author ACER
+ */
+public class ArrStringHari {
+    public static void main(String[] args) {
+        String hari[];
+        hari = new String[7];
+        
+        hari[0] = "Senin";
+        hari[1] = "Selasa";
+        hari[2] = "Rabu";
+        hari[3] = "Kamis";
+        hari[4] = "Jumat";
+        hari[5] = "Sabtu";
+        hari[6] = "Minggu";
+    }
+}
